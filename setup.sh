e43c2c164d7256b0c8a497e0b72dd236d2c48d18
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2024-10-23 19:30:30 -0400

    Update and rename setup.txt to setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+cd ~
+sudo apt update -y && sudo apt upgrade -y
+# install/config git
+sudo apt install git -y
+git config --global user.name jpatrick5402
+git config --global user.email jpatrick5402@gmail.com
+# add dotfiles
+git clone https://github.com/jpatrick5402/dotfiles.git
+mkdir .config/
+ln -s ~/dotfiles/nvim/ ~/.config/nvim
+# build neovim: follow https://github.com/neovim/neovim/blob/master/BUILD.md
+sudo apt-get install ninja-build gettext cmake unzip curl build-essential\
+sudo apt install npm -y
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+# install z
+git clone https://github.com/rupa/z.git
+echo ". /home/jpatrick5402/z/z.sh" >> .bashrc
