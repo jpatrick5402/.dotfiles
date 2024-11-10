393fab66675477095f5f37d3fb94d5c18f5a647a
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2024-11-09 20:08:57 -0500

    Update setup_arch.sh

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,1 +1,22 @@
-
+cd
+sudo pacman -Syu
+# install/config git
+sudo pacman -S git
+git config --global user.name jpatrick5402
+git config --global user.email jpatrick5402@gmail.com
+# add dotfiles
+git clone https://github.com/jpatrick5402/dotfiles.git
+mkdir .config/
+ln -s ~/dotfiles/nvim/ ~/.config/nvim
+# build neovim: follow https://github.com/neovim/neovim/blob/master/BUILD.md
+sudo pacman -S base-devel cmake unzip ninja curl npm
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+# install z
+git clone https://github.com/rupa/z.git
+echo ". /home/jpatrick5402/z/z.sh" >> .bashrc
+# install fzf
+sudo pacman -S fzf
+# customize prompt
+echo 'PS1="┌─[\[\e[01;32m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[1;34m\]\w\[\e[0m\]]\n└─╼ "' >> .bashrc
