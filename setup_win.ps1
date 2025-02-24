de99d1cb1c14aaa3cd10cf87ba397786ac9c5a48
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2025-02-24 12:40:43 -0500

    Update setup_win.ps1

--- setup_win.ps1
+++ setup_win.ps1
@@ -3,5 +3,8 @@
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# Install NVIM
+choco install neovim
 choco install mingw
 choco install fzf
+winget install --id OpenJS.NodeJS
