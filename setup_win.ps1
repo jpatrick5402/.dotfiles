61b7b03854df2994df12ca93e3f84f92c25c9e96
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2025-02-24 16:02:33 -0500

    Update setup_win.ps1

--- setup_win.ps1
+++ setup_win.ps1
@@ -1,9 +1,11 @@
+cd ~
+git clone https://github.com/jpatrick5402/dotfiles
 # SFTP Drive - https://www.callback.com/sftpdrive
-
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
 # Install NVIM
+cd ~
+New-Item -ItemType SymbolicLink -Path C:\Users\$env:username\AppData\Local\nvim -Target C:\Users\$env:username\dotfiles\nvim\
 choco install neovim
 choco install mingw
 choco install fzf
