83c72ba600c7f8c32c53a901e6c3a42f87d54d00
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2025-02-24 09:12:55 -0500

    Update setup_win.ps1

--- setup_win.ps1
+++ setup_win.ps1
@@ -1,1 +1,7 @@
 # SFTP Drive - https://www.callback.com/sftpdrive
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install mingw
+choco install fzf
