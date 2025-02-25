409a3d7d23926f2dbb3d05538e9f24c5fe4531ba
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2025-02-25 08:14:22 -0500

    Update setup_win.ps1

--- setup/setup_win.ps1
+++ setup/setup_win.ps1
@@ -7,7 +7,7 @@ cd ~
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install Git
-choco install git -y
+choco install git -y --force
 
 # Pull dotfiles into home dir
 cd ~
@@ -16,20 +16,20 @@ git clone https://github.com/jpatrick5402/dotfiles
 # Install NVIM
 cd ~
 New-Item -ItemType SymbolicLink -Path .\AppData\Local\nvim -Target .\dotfiles\nvim\
-choco install neovim -y
-choco install mingw -y
-choco install fzf -y
-choco install nodejs -y
+choco install neovim -y --force
+choco install mingw -y --force
+choco install fzf -y --force
+choco install nodejs -y --force
 
 # Set up AutoHotKey
-choco install autohotkey -y
+choco install autohotkey -y --force
 # Create AutoHotKey shortcut in startup folder
 cd ~
 $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut(".\AppData\Microsoft\Windows\Start Menu\Programs\Startup\AutoHotKey Main.lnk")
 $shortcut.TargetPath = "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe"
-$shortcut.Arguments = "`"$env:USERPROFILE\dotfiles\main.ahk`""
+$shortcut.Arguments = "`".\dotfiles\main.ahk`""
 $shortcut.Save()
 
 # Install Vivaldi
-choco install Vivaldi -y
+choco install Vivaldi -y --force
