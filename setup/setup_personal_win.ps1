6be1bfee9d5b8c3e718331e8bcdcd97c83ca88b1
Joseph Patrick <jpatrick5402@gmail.com>
2025-02-26 18:46:25 -0500

    Adjustments

--- setup/setup_personal_win.ps1
+++ setup/setup_personal_win.ps1
@@ -1,6 +1,9 @@
 # Install steam
 choco install steam -y
 
+# Install StartAllBack
+choco install StartAllBack -y
+
 # Set up sshfs
 $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut(".\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Connect Pi.lnk")
