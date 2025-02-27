f0b06430b875b0abeb1975bb5d58540dfbf3bb37
jpatrick5402 <jpatrick5402@gmail.com>
2025-02-26 18:17:42 -0800

    Adding git fix, adding decision

--- setup/setup_main_win.ps1
+++ setup/setup_main_win.ps1
@@ -6,7 +6,24 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 # Install Git
 choco install git -y
 
-# Need to create some way to restart shell here so git can be used
+# Process to restart shell if git is not yet recognized as env var
+function Restart-PowerShell {
+  Start-Process powershell.exe -ArgumentList "-NoExit", "& '$($Script:MyInvocation.MyCommand.Path)'" -Verb RunAs
+  exit
+}
+try {
+    & git --version | Out-Null
+    Write-Host "Git is installed (found in PATH)."
+    $gitVersion = & git --version
+    Write-Host $gitVersion
+}
+catch {
+    Restart-PowerShell
+}
+
+# Configure git
+git config --global user.email "jpatrick5402@gmail.com"
+git config --global user.name "jpatrick5402"
 
 # Pull dotfiles into home dir
 cd ~
@@ -53,3 +70,30 @@ choco install logioptionsplus -y
 
 # Install sshfs
 choco install sshfs -y
+
+# Decide if to perform personal install
+function Confirm-Continue {
+  param (
+    [string]$Prompt = "Do you want to perform personal installations? (y/n)"
+  )
+
+  while ($true) {
+    $response = Read-Host -Prompt $Prompt
+
+    if ($response -eq "y" -or $response -eq "Y") {
+      return $true
+    } elseif ($response -eq "n" -or $response -eq "N") {
+      return $false
+    } else {
+      Write-Host "Invalid input. Please enter 'y' or 'n'."
+    }
+  }
+}
+
+if (Confirm-Continue) {
+    Write-Host "Continuing..."
+    Start-Process powershell.exe -ArgumentList "-NoExit", "$env:USERPROFILE\dotfiles\setup\setup_personal_win.ps1" -Verb RunAs
+} else {
+    Write-Host "Exiting..."
+  exit
+}
