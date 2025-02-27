0345281e7793c900d644ce920c7213534ce01ac5
jpatrick5402 <jpatrick5402@gmail.com>
2025-02-26 23:35:10 -0500

    Adding back autoconnect shortcut

--- setup/setup_win.ps1
+++ setup/setup_win.ps1
@@ -92,4 +92,8 @@ choco install steam -y
 choco install StartAllBack -y
 
 # Set up sshfs
-Start-Process powershell.exe -ArgumentList "-NoExit", "$env:USERPROFILE\dotfiles\scripts\AutoConnectPi.ps1" -Verb RunAs
+$shell = New-Object -ComObject WScript.Shell
+$shortcut = $shell.CreateShortcut(".\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Connect Pi.lnk")
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "`"$env:USERPROFILE\dotfiles\scripts\AutoConnectPi.ps1`""
+$shortcut.Save()
