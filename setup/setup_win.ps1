01d8ba1c740edd46698fcfdcbaa3d8db3c622d81
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2025-03-04 15:37:31 -0800

    Update setup_win.ps1

--- setup/setup_win.ps1
+++ setup/setup_win.ps1
@@ -98,8 +98,8 @@ if (Confirm-Continue) {
     # Install/Setup Mouse tool
     choco install mouse-jiggler -y
     $shortcut = $shell.CreateShortcut(".\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Mouse.lnk")
-    $shortcut.TargetPath = "powershell.exe"
-    $shortcut.Arguments = "`"$env:USERPROFILE\dotfiles\scripts\AutoConnectPi.ps1`""
+    $shortcut.TargetPath = "`"C:\ProgramData\chocolatey\bin\MouseJiggler.exe`""
+    $shortcut.Arguments = "`"-jzm`""
     $shortcut.Save()
 }
 
