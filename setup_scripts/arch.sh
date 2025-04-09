e9a1590702db5c5829923bc4d90be72a815282c1
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2025-04-08 22:28:00 -0400

    Update arch.sh

--- setup_scripts/arch.sh
+++ setup_scripts/arch.sh
@@ -6,9 +6,6 @@ sudo pacman -Syu --noconfirm
 # install basic apps
 sudo pacman -S npm zsh arandr blueman polybar picom feh git alacritty neovim base-devel fzf ttf-3270-nerd ttf-firacode-nerd firefox man clipmenu less bluez bluez-utils pulseaudio pulseaudio-bluetooth pulseaudio-jack --noconfirm
 
-# adding zsh-suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 # add dotfiles (in case they're not already loaded
 git clone https://github.com/jpatrick5402/.dotfiles
 
@@ -40,6 +37,9 @@ done
 
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# adding zsh-suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # create symlinks for dotfiles
 DIR=${HOME}/.dotfiles/nix_configs
