a4640e9719170b57faf4e00a90dc4e20af94a31e
jpatrick5402 <jpatrick5402@gmail.com>
2025-04-14 09:15:46 -0400

    Fixing monitors 2

--- setup_scripts/arch.sh
+++ setup_scripts/arch.sh
@@ -4,7 +4,7 @@ cd ${HOME}
 sudo pacman -Syu --noconfirm
 
 # install basic apps
-sudo pacman -S ripgrep rofi npm zsh arandr blueman polybar picom feh git alacritty neovim base-devel fzf ttf-3270-nerd ttf-firacode-nerd firefox man less bluez bluez-utils pulseaudio pulseaudio-bluetooth pulseaudio-jack --noconfirm
+sudo pacman -S zip unzip autorandr ripgrep rofi npm zsh arandr blueman polybar picom feh git alacritty neovim base-devel fzf ttf-3270-nerd ttf-firacode-nerd firefox man less bluez bluez-utils pulseaudio pulseaudio-bluetooth pulseaudio-jack --noconfirm
 
 # add dotfiles (in case they're not already loaded
 git clone https://github.com/jpatrick5402/.dotfiles
