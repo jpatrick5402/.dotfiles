8c7fc39b0df35e823df3563f6329bfa2cfffeee9
jpatrick5402 <jpatrick5402@gmail.com>
2025-04-14 14:50:34 -0400

    Fixing NVIDIA drivers

--- scripts/f_screen.sh
+++ scripts/f_screen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --mode 1920x1080 --rate 165 --pos 0x180 --rotate normal --output HDMI-1 --off --output DP-2 --primary --mode 2560x1440 --rate 165 --pos 1920x0 --rotate normal --output HDMI-2 --off --output DP-3 --off
+xrandr --output DP-0 --mode 1920x1080 --rate 165 --pos 0x180 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --primary --mode 2560x1440 --rate 165 --pos 1920x0 --rotate normal --output HDMI-2 --off --output DP-3 --off
