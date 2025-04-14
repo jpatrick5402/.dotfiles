94f2d16bbf1e79888b41a1fa8f5bfe218f92ecf6
jpatrick5402 <jpatrick5402@gmail.com>
2025-04-14 09:14:10 -0400

    Fixing monitors

---
+++ scripts/f_screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --primary --mode 1920x1080 --rate 165 --pos 0x180 --rotate normal --output HDMI-1 --off --output DP-2 --mode 2560x1440 --rate 165 --pos 1920x0 --rotate normal --output HDMI-2 --off --output DP-3 --off
