76cb629f588ae13c6ecdafed5a246849e19b4328
jpatrick5402 <jpatrick5402@gmail.com>
2025-04-16 21:46:54 -0400

    Only one bar

--- scripts/launch_polybar.sh
+++ scripts/launch_polybar.sh
@@ -1,7 +1,8 @@
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
-  done
-else
-  polybar --reload main &
-fi
+polybar
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload main &
+#  done
+#else
+#  polybar --reload main &
+#fi
