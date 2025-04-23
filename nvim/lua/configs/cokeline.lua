24ca0b467fe0bd71e6b69cf31f0895285d39cfa6
jpatrick5402 <jpatrick5402@gmail.com>
2025-04-23 12:13:10 -0400

    More configs

--- nvim/lua/configs/cokeline.lua
+++ nvim/lua/configs/cokeline.lua
@@ -1,31 +1,19 @@
+local is_picking_focus = require("cokeline.mappings").is_picking_focus
+local is_picking_close = require("cokeline.mappings").is_picking_close
 local get_hex = require("cokeline.hlgroups").get_hl_attr
+local hlgroups = require("cokeline.hlgroups")
+local hl_attr = hlgroups.get_hl_attr
 
+local red = vim.g.terminal_color_1
+local green = vim.g.terminal_color_2
 local yellow = vim.g.terminal_color_3
 
 require("cokeline").setup({
 	default_hl = {
 		fg = function(buffer)
-			return buffer.is_focused and get_hex("Normal", "fg") or get_hex("Comment", "fg")
+			return buffer.is_focused and hl_attr("Normal", "fg") or hl_attr("Comment", "fg")
 		end,
-		bg = function()
-			return get_hex("ColorColumn", "bg")
-		end,
-	},
-
-	sidebar = {
-		filetype = { "NvimTree", "neo-tree" },
-		components = {
-			{
-				text = function(buf)
-					return buf.filetype
-				end,
-				fg = yellow,
-				bg = function()
-					return get_hex("NvimTreeNormal", "bg")
-				end,
-				bold = true,
-			},
-		},
+		bg = hl_attr("ColorColumn", "bg"),
 	},
 
 	components = {
@@ -35,70 +23,21 @@ require("cokeline").setup({
 			end,
 		},
 		{
-			text = "  ",
-		},
-		{
-			text = function(buffer)
-				return buffer.devicon.icon
-			end,
-			fg = function(buffer)
-				return buffer.devicon.color
-			end,
-		},
-		{
 			text = " ",
 		},
 		{
 			text = function(buffer)
-				return buffer.filename .. "  "
-			end,
-			bold = function(buffer)
-				return buffer.is_focused
-			end,
-		},
-		{
-			text = "",
-			on_click = function(_, _, _, _, buffer)
-				buffer:delete()
-			end,
-		},
-		{
-			text = "  ",
-		},
-	},
-	default_hl = {
-		fg = function(buffer)
-			return buffer.is_focused and get_hex("Normal", "fg") or get_hex("Comment", "fg")
-		end,
-		bg = get_hex("ColorColumn", "bg"),
-	},
-
-	components = {
-		{
-			text = "｜",
-			fg = function(buffer)
-				return buffer.is_modified and yellow or green
-			end,
-		},
-		{
-			text = function(buffer)
-				return buffer.devicon.icon .. " "
+				return (is_picking_focus() or is_picking_close()) and buffer.pick_letter .. " " or buffer.devicon.icon
 			end,
 			fg = function(buffer)
-				return buffer.devicon.color
+				return (is_picking_focus() and yellow) or (is_picking_close() and red) or buffer.devicon.color
 			end,
-		},
-		{
-			text = function(buffer)
-				return buffer.index .. ": "
+			italic = function()
+				return (is_picking_focus() or is_picking_close())
 			end,
-		},
-		{
-			text = function(buffer)
-				return buffer.unique_prefix
+			bold = function()
+				return (is_picking_focus() or is_picking_close())
 			end,
-			fg = get_hex("Comment", "fg"),
-			italic = true,
 		},
 		{
 			text = function(buffer)
@@ -106,6 +45,12 @@ require("cokeline").setup({
 			end,
 			bold = function(buffer)
 				return buffer.is_focused
+			end,
+		},
+		{
+			text = "",
+			on_click = function(_, _, _, _, buffer)
+				buffer:delete()
 			end,
 		},
 		{
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,10 +3,6 @@ vim.keymap.set("n", "<space>e", require("nvim-tree.api").tree.toggle, {
 	desc = "toggle nvim-tree",
 })
 
-vim.keymap.set("n", "<space>bp", "<cmd>BufferLinePick<CR>", {
-	desc = "pick a buffer",
-})
-
 vim.keymap.set("n", ";", ":", {
 	desc = "command mode",
 })
