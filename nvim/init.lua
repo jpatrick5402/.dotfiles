8d8bb5e95a2952d8fbf74ae77a208f1c9fa2df5d
jpatrick5402 <jpatrick5402@gmail.com>
2025-04-24 12:44:13 -0400

    Adding format toggle

--- nvim/init.lua
+++ nvim/init.lua
@@ -752,22 +752,29 @@ require("lazy").setup({
 				mode = "",
 				desc = "[F]ormat buffer",
 			},
+			{
+				"<leader>tf",
+				function()
+					if vim.g.disable_autoformat then
+						vim.cmd([[FormatEnable]])
+						vim.notify("Formatting Enabled")
+					else
+						vim.cmd([[FormatDisable]])
+						vim.notify("Formatting Disabled")
+					end
+				end,
+				mode = "",
+				desc = "[T]oggle [F]ormat",
+			},
 		},
 		opts = {
 			notify_on_error = false,
 			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					return nil
-				else
-					return {
-						timeout_ms = 500,
-						lsp_format = "fallback",
-					}
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
 				end
+				return { timeout_ms = 500, lsp_format = "fallback" }
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
@@ -1016,3 +1023,21 @@ require("lazy").setup({
 -- Joseph's configs
 require("mappings")
 require("autocmds")
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
