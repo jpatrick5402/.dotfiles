8d4aa34f509226b1a824182b36bdc49d510a71f5
jpatrick5402 <jpatrick5402@gmail.com>
2025-05-16 23:15:09 -0400

    more configs

--- scripts/f_screen.sh
+++ scripts/f_screen.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-xrandr --output DP-0 --mode 1920x1080 --rate 165 --pos 0x180 --rotate normal --output DP-2 --primary --mode 2560x1440 --rate 165 --pos 1920x0 --rotate normal
+#!/bin/bash
+xrandr --output DP-0 --primary --mode 2560x1440 --rate 165 --pos 1920x0 --output DP-2 --mode 1920x1080 --rate 165 --pos 0x180
--- setup_scripts/arch_awesome.sh
+++ setup_scripts/arch_awesome.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #update system
 sudo pacman -Syu
 
@@ -5,7 +6,20 @@ sudo pacman -Syu
 ln -sf ~/.dotfiles/nvim/ ~/.config/
 
 #install basic packages
-sudo pacman -S man numlockx neovim ttf-firacode-nerd --noconfirm
+sudo pacman -S btop zip unzip gcc ripgrep tree man numlockx neovim ttf-firacode-nerd base-devel --noconfirm
 
 #install config-specific packages
-sudo pacman -S picom polybar feh rofi --noconfirm
+sudo pacman -S dmenu zsh picom rofi --noconfirm
+
+#install yay
+if pacman -Qs yay > /dev/null; then echo "Yay is installed"
+else
+  cd && git clone https://aur.archlinux.org/yay.git && cd yay
+  makepkg -si --noconfirm && cd
+fi
+
+#install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# adding zsh-suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
