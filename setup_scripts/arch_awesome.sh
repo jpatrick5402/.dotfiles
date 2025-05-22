8aad37c962327e6a71ee7b054c204d910c668d7d
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2025-05-22 13:58:52 -0400

    Update arch_awesome.sh

--- setup_scripts/arch_awesome.sh
+++ setup_scripts/arch_awesome.sh
@@ -34,3 +34,6 @@ done
 
 #install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
