392e2f770091e481c5735daa4f46d54ca5f959c6
Joseph Patrick <jpatrick5402@gmail.com>
2025-09-19 17:37:27 -0400

    Adding fish

--- configs_linux/v2/installs.sh
+++ configs_linux/v2/installs.sh
@@ -1,1 +1,7 @@
-sudo pacman -S mako qt5-wayland qt6-wayland waybar hyprpaper wl-clipboard wl-clip-persist imagemagick hyprlock hypridle btop
+sudo pacman -S mako qt5-wayland qt6-wayland waybar hyprpaper wl-clipboard wl-clip-persist imagemagick hyprlock hypridle btop fish
+
+cd ~
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
