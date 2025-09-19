4f01e5469b2c16cc343d4459dcd4bca3a7701af1
Joseph Patrick <jpatrick5402@gmail.com>
2025-09-19 16:37:41 -0400

    minor changes

--- configs_linux/v2/installs.sh
+++ configs_linux/v2/installs.sh
@@ -1,1 +1,1 @@
-sudo pacman -S mako qt5-wayland qt6-wayland waybar hyprpaper wl-clipboard wl-clip-persist imagemagick 
+sudo pacman -S mako qt5-wayland qt6-wayland waybar hyprpaper wl-clipboard wl-clip-persist imagemagick hyprlock hypridle btop
