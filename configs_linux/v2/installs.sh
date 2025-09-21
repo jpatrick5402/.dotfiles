d1cb69c71575b55e76685e637a65dd5f6fd3e8a3
Joseph Patrick <jpatrick5402@gmail.com>
2025-09-20 22:33:23 -0400

    power menu + style

--- configs_linux/v2/installs.sh
+++ configs_linux/v2/installs.sh
@@ -1,4 +1,4 @@
-sudo pacman -S mako qt5-wayland qt6-wayland waybar hyprpaper wl-clipboard wl-clip-persist imagemagick hyprlock hypridle btop fish
+sudo pacman -S mako qt5-wayland qt6-wayland waybar hyprpaper wl-clipboard wl-clip-persist imagemagick hyprlock hypridle btop fish wf-recorder
 
 cd ~
 sudo pacman -S --needed git base-devel
