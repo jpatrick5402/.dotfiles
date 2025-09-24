251c35a2e31f1e5914ec30fd3149c57fea9505b5
Joseph Patrick <jpatrick5402@gmail.com>
2025-09-24 09:08:40 -0400

    simple configs

--- configs_linux/v2/installs.sh
+++ configs_linux/v2/installs.sh
@@ -1,5 +1,7 @@
 sudo pacman -S mako qt5-wayland qt6-wayland waybar hyprpaper wl-clipboard wl-clip-persist imagemagick hyprlock hypridle btop fish wf-recorder
 
+# Make sure if using Nvidia GPU, you install the below package (https://wiki.archlinux.org/title/NVIDIA)
+# sudo pacman -S nvidia
 cd ~
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
