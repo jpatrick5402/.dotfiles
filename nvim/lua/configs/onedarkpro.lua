ab8e3400705e887652a4008ee1ef0e06e520b60d
Joseph Patrick <jpatrick5402@gmail.com>
2026-04-11 23:54:24 -0400

    resetting nvim

--- nvim/lua/configs/onedarkpro.lua
+++
@@ -1,5 +0,0 @@
-require("onedarkpro").setup({
-	options = {
-		transparency = true,
-	},
-})
---
+++ nvim/lua/plugins/onedarkpro.lua
@@ -0,0 +1,13 @@
+return {
+	{
+        'olimorris/onedarkpro.nvim',
+        priority = 1000, -- Ensure it loads first
+        config = function()
+			require("onedarkpro").setup({
+				options = {
+					transparency = false,
+				},
+			})
+        end,
+    },
+}
